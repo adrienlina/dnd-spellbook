ed988bb87013ec9338685abdf833392f392b2fda
Adrien Lina <adrienlina@gmail.com>
2017-12-12 10:46:31 +0100

    Add tests to spellbook models

---
+++ src/spellbook/tests/test_spell_model.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from spellbook.models import Spell
+
+
+class SpellModelCase(TestCase):
+    def test_0_create_spell(self):
+        """It should be possible to create a spell"""
+        n_spells = Spell.objects.count()
+
+        new_spell = Spell(
+            name="spell_name",
+            level=0,
+            cast_type="cast_type",
+            spell_range="spell_range",
+            description="description",
+        )
+
+        new_spell.save()
+
+        self.assertEqual(
+            Spell.objects.count(),
+            n_spells + 1
+        )
---
+++ src/spellbook/tests/test_spellbook_model.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from profiles.models import Profile
+from spellbook.models import Spellbook
+
+User = get_user_model()
+
+class SpellbookModelCase(TestCase):
+    def test_0_create_spellbook(self):
+        """It should be possible to create a spell"""
+
+        # We need a profile for a spellbook, thus we need a django user
+        user = User(email="email@email.com", password="password")
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        n_spellbooks = Spellbook.objects.count()
+
+        new_spell_book = Spellbook(
+            name="spellbook_name",
+            profile=profile,
+        )
+
+        new_spell_book.save()
+
+        self.assertEqual(
+            Spellbook.objects.count(),
+            n_spellbooks + 1
+        )
